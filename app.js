3d32e9de1d356e026459e060eb7d1b08af001ca1
Sepideh Attar <sepideh.att@gmail.com>
2022-06-01 16:44:25 +0200

    Added user route

--- app.js
+++ app.js
@@ -14,10 +14,13 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+const { isAuthenticated } = require('./middleware/jwt.middleware')
+
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 app.use("/api", require("./routes/index.routes"));
 app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/users",isAuthenticated, require("./routes/user.routes"));
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
