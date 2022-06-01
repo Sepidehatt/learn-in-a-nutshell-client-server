512417daf56d94d28f9a042295c75a1599908d87
Sepideh Attar <sepideh.att@gmail.com>
2022-06-01 17:44:59 +0200

    added get summery list-summery detail and update summery

--- routes/summery.routes.js
+++ routes/summery.routes.js
@@ -32,10 +32,60 @@ router.post('/:bookId', (req, res, next) => {
 
 //Get Summeries list
 
+router.get('/',(req,res,next)=>{
+  Summery.find()
+  .populate('summeryOwner')
+  .populate('refrenceBook')
+  .then(response => res.status(201).json(response))
+  .catch(err => {
+      console.log("error getting summeries list in the DB", err);
+      res.status(500).json({
+          message: "error getting summeries list in the DB",
+          error: err
+      });
+  })
+})
+
 //get Summery detail
+
+router.get('/:summeryId',(req,res,next)=>{
+  const {summeryId}=req.params
+  Summery.findById(summeryId)
+  .populate('summeryOwner')
+  .populate('refrenceBook')
+  .then(response => res.status(201).json(response))
+  .catch(err => {
+      console.log("error getting a summery in the DB", err);
+      res.status(500).json({
+          message: "error getting a summery in the DB",
+          error: err
+      });
+  })
+})
 
 
 //Edit Summery
+router.put('/:summeryId/edit', (req, res, next) => { 
+  const { summeryId } = req.params;
+
+  Summery.findById(summeryId)
+      .then(summeryDetails => {
+          if (summeryDetails.summeryOwner == req.payload._id) {
+              return Summery.findByIdAndUpdate(summeryId, req.body, { new: true })
+                  .then((updatedSummery) => res.status(201).json(updatedSummery))
+                  .catch(err => {
+                      console.log("error updating summery", err);
+                      res.status(500).json({
+                          message: "error updating summery",
+                          error: err
+                      });
+                  })
+          } else {
+              res.status(400).json({ message: 'user is not the onwer of this summery' })
+          }
+      })
+
+});
 
 //like summery
 
